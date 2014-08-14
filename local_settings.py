826d760a4eb6a58033d4e334a6716c3b6b0f8eaa
mathhomework <toxstalks@gmail.com>
2014-08-13 20:58:55 -0700

    first commit

---
+++ local_settings.py
@@ -0,0 +1,8 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookmaps',
+        'USER': 'postgres',
+        'PASSWORD': 'hotdog'
+    }
+}
