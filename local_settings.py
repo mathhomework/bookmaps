6d4b7eb9e8231cf60e3c83e4862cb97fea667866
Rudy Mutter <rudy@yetihq.com>
2014-08-22 12:16:33 -0700

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,5 @@
+# You need to use a requirements.txt and .gitignore file, lots of files that shouldn't be checked in to your repo (.pyc files)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
